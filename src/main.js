70db8ed8fa9bf16506d6a499498b9170ab71bebd
Wilhelm Kampelmann <wilhelm.kampelmann@gmail.com>
2020-11-14 18:42:01 +0100

    main.js debugged

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
 import App from './App.vue';
 import router from './router';
 
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
-vue.use(Buefy)
-
 Vue.config.productionTip = false;
 
+Vue.use(Buefy);
 new Vue({
   router,
   render: (h) => h(App),
