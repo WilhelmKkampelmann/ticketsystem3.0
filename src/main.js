8896b580b6667369f4b04a5b03924b24d86648a2
Wilhelm Kampelmann <wilhelm.kampelmann@gmail.com>
2020-11-20 15:03:47 +0100

    vue add i18n

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 import App from './App.vue';
 import router from './router';
+import i18n from './i18n';
 
 Vue.config.productionTip = false;
 
 Vue.use(Buefy);
 new Vue({
   router,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
